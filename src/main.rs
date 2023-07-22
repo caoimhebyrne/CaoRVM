d86a8a0273c48e70f5b069ca2250d6230aee6474
Caoimhe <caoimhebyrne06@gmail.com>
2023-07-22 22:30:17 +0100

    ClassParser: Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ struct HexValue {
     value: u32,
 }
 
+trait HexValueExt {
+    fn hex(self) -> HexValue;
+}
+
+impl HexValueExt for u32 {
+    fn hex(self) -> HexValue {
+        HexValue { value: self }
+    }
+}
+
 impl fmt::Debug for HexValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:#02X}", self.value)
@@ -23,17 +33,30 @@ struct ClassFile {
 
 #[derive(Debug)]
 enum ClassParserError {
-    UnableToRead,
+    InvalidMagic(HexValue),
+    FailedToRead,
+    UnexpectedEOF,
+}
+
+impl<V> From<ClassParserError> for Result<V, ClassParserError> {
+    fn from(error: ClassParserError) -> Self {
+        Err(error)
+    }
 }
 
 fn parse_class_file(file: &mut Vec<u8>) -> Result<ClassFile, ClassParserError> {
-    let magic = read_u4(file).ok_or(ClassParserError::UnableToRead)?;
-    let minor_version = read_u2(file).ok_or(ClassParserError::UnableToRead)?;
-    let major_version = read_u2(file).ok_or(ClassParserError::UnableToRead)?;
-    let constant_pool_count = read_u2(file).ok_or(ClassParserError::UnableToRead)?;
+    let magic = read_u4(file)?;
+    if magic != 0xCAFEBABE {
+        return ClassParserError::InvalidMagic(magic.hex()).into();
+    }
+
+    let minor_version = read_u2(file)?;
+    let major_version = read_u2(file)?;
+
+    let constant_pool_count = read_u2(file)?;
 
     let file = ClassFile {
-        magic: HexValue { value: magic },
+        magic: magic.hex(),
         minor_version,
         major_version,
         constant_pool_count,
@@ -47,11 +70,6 @@ fn main() {
 
     match parse_class_file(&mut file) {
         Ok(parsed) => {
-            if parsed.magic.value != 0xCAFEBABE {
-                eprintln!("Magic {:#?} didn't equal 0xCAFEBABE!", parsed.magic);
-                return;
-            }
-
             if parsed.major_version != 52 || parsed.minor_version != 0 {
                 eprintln!(
                     "Expected major version to be 52 (got {}) and minor version to be 0 (got {})!",
@@ -59,37 +77,45 @@ fn main() {
                 );
             }
 
-            println!("Parsed class file: {:#?}", parsed)
+            println!("Parsed class file: {:?}", parsed)
         }
-        Err(error) => eprintln!("Failed to parse class file!\n{:#?}", error),
+        Err(error) => eprintln!("Failed to parse class file: {:?}", error),
     }
 }
 
-fn read_u2(file: &mut Vec<u8>) -> Option<u16> {
+fn read_u1(file: &mut Vec<u8>) -> Result<u8, ClassParserError> {
     if file.is_empty() {
-        return None;
+        return ClassParserError::UnexpectedEOF.into();
+    }
+
+    let array: Vec<u8> = read_n_bytes(file, 1);
+    match array.try_into() {
+        Ok(value) => Ok(u8::from_be_bytes(value)),
+        Err(_) => ClassParserError::FailedToRead.into(),
+    }
+}
+
+fn read_u2(file: &mut Vec<u8>) -> Result<u16, ClassParserError> {
+    if file.is_empty() {
+        return ClassParserError::UnexpectedEOF.into();
     }
 
     let array: Vec<u8> = read_n_bytes(file, 2);
     match array.try_into() {
-        Ok(value) => Some(u16::from_be_bytes(value)),
-
-        // TODO: Error forwarding?
-        Err(_) => None,
+        Ok(value) => Ok(u16::from_be_bytes(value)),
+        Err(_) => ClassParserError::FailedToRead.into(),
     }
 }
 
-fn read_u4(file: &mut Vec<u8>) -> Option<u32> {
+fn read_u4(file: &mut Vec<u8>) -> Result<u32, ClassParserError> {
     if file.is_empty() {
-        return None;
+        return ClassParserError::UnexpectedEOF.into();
     }
 
     let array: Vec<u8> = read_n_bytes(file, 4);
     match array.try_into() {
-        Ok(value) => Some(u32::from_be_bytes(value)),
-
-        // TODO: Error forwarding?
-        Err(_) => None,
+        Ok(value) => Ok(u32::from_be_bytes(value)),
+        Err(_) => ClassParserError::FailedToRead.into(),
     }
 }
 
